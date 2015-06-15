6951808fb2fd12cfbb66c1c38e95b7f93036677c
Tsung Hung <thung@me.com>
2015-06-14 18:45:14 -0700

    updated index with route of api/tweets/ and userid handle logic

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 var express = require('express');
+var fixtures  = require('./fixtures');
 
 var app = express();
 
 app.get('/api/tweets', function (req, res) {
-  res.send('Hello World!');
+  var userId = req.query.userId;
+
+  if (!userId) {
+    return res.sendStatus(400);
+  }
+
+  var tweets = [];
+  for (var i = 0; i < fixtures.tweets.length; i++) {
+    if (fixtures.tweets[i].userId === userId) {
+      tweets.push(fixtures.tweets[i]);
+    }
+  }
+
+  var tweetsSorted = tweets.sort(function(t1, t2) {
+    if (t1.created > t2.created) {
+      return -1;
+    } else if (t1.created === t2. created) {
+      return 0;
+    } else {
+      return 1;
+    }
+  });
+
+  return res.send({
+    tweets: tweetsSorted
+  });
 });
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+var server = app.listen(3000, "127.0.0.1", function () {
+  console.log('App Listening @ http://127.0.0.1:3000');
 
 });
 
