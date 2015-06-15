fd9e19520c828e6937fc36fef81a99a4c5183f38
Tsung Hung <thung@me.com>
2015-06-14 18:28:20 -0700

    first commit

---
+++ hello_server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello!');
+}).listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
+
+module.exports = server;
