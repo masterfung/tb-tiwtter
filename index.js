96526f13c09778b4507315cddc638f340226f685
Tsung Hung <thung@me.com>
2015-06-15 18:10:52 -0700

    brought in cookie parser, session, passport

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session  = require('express-session');
 var shortId = require('shortid');
 var _ = require('lodash');
-// var LocalStrategy = require('passport-local').Strategy;
+
 
 
 var fixtures  = require('./fixtures');
-var auth = require('./auth');
+var passport = require('./auth');
 var routes = require('./routes');
 
 var app = express();
 
 app.use(bodyParser.json());
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(cookieParser());
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
-
 
 var server = app.listen(3000, "127.0.0.1", function () {
   console.log('App Listening @ http://127.0.0.1:3000');
