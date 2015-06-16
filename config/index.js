9151a2eef82a98e39334dec454ed0f550ac48b1c
Tsung Hung <thung@me.com>
2015-06-15 21:26:20 -0700

    nconf and config for server type based on node env

---
+++ config/index.js
@@ -0,0 +1,14 @@
+var nconf = require('nconf');
+var path = require('path')
+
+nconf.env()
+
+if (nconf.get('NODE_ENV') === 'dev') {
+  nconf.file(path.join(__dirname, 'config-dev.json'));
+} else if (nconf.get('NODE_ENV') === 'test') {
+  nconf.file(path.join(__dirname, 'config-test.json'));
+} else if (nconf.get('NODE_ENV') === 'prod'){
+  nconf.file(path.join(__dirname, 'config-prod.json'));
+}
+
+module.exports = nconf;
